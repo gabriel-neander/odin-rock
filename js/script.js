6359bc1de8141833ab57a18d5a599ee270f92594
Gabriel Neander <114587036+gabriel-neander@users.noreply.github.com~git config --global user.email 114587036+gabriel-neander@users.noreply.github.com~>
2023-08-26 13:25:07 -0300

    Make game playing on console.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-// Random number to support computer choice */
+// Setting vars
+let pcResult;
+let playerChoice;
+
+// Random number to support computer choice
 function randomizer() {
     let randomic = Math.floor(Math.random() * 3);
     return randomic;
 }
-// Function to actually make a choice for computer */
+
+// Function to actually make a choice for computer
 function getComputerChoice() {
     let result;
     if (randomizer() === 0) {
@@ -15,21 +20,49 @@ function getComputerChoice() {
     }
     return result;
 }
-let pcResult = getComputerChoice(),
-plSelect = prompt('Choose your destiny (Rock/Paper/Scissors):')
-plSelect = plSelect.charAt(0).toUpperCase() + plSelect.slice(1).toLowerCase();
-    function playGame(playerSelection = plSelect, computerSelection = pcResult) {
-        let message = "";
-        if (playerSelection === computerSelection) {
-            message = 'It is a draw!';
-        } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-                   (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-                   (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-            message = 'You win!';
-        } else {
-            message = 'You lose!';
-        }
-        return message;
+
+// Function to get player's choice
+function getPlayerChoice(callback) {
+    const buttonRock = document.querySelector('#buttonRock');
+    const buttonPaper = document.querySelector('#buttonPaper');
+    const buttonScissors = document.querySelector('#buttonScissors');
+
+    buttonRock.addEventListener('click', () => {
+        callback('Rock');
+    });
+    
+    buttonPaper.addEventListener('click', () => {
+        callback('Paper');
+    });
+
+    buttonScissors.addEventListener('click', () => {
+        callback('Scissors');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getPlayerChoice(function(choice) {
+        // Defining results after playerChoice is set
+        playerChoice = choice;
+        pcResult = getComputerChoice();
+
+        // Playing game
+        let message = playGame(playerChoice, pcResult);
+        console.log("PC plays: " + pcResult + " and you play: " + playerChoice + " so, the result is " + message);
+    });
+});
+
+// Game scope
+function playGame(playerChoice, pcResult) {
+    let message = "";
+    if (playerChoice === pcResult) {
+        message = 'It is a draw!';
+    } else if ((playerChoice === 'Rock' && pcResult === 'Scissors') ||
+               (playerChoice === 'Paper' && pcResult === 'Rock') ||
+               (playerChoice === 'Scissors' && pcResult === 'Paper')) {
+        message = 'You win!';
+    } else {
+        message = 'You lose!';
     }
-// Show results in console
-    console.log("Your call: " + plSelect + " PC Call: " + pcResult + " | Result: " + playGame());
+    return message;
+}
